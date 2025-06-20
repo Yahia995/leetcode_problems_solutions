cab70c4280514dd59860df1dcc037cff0ab393df
Yahia Achouri <yahia995ach@gmail.com>
2025-06-20 13:33:14 +0100

    Time: 0 ms (100.00%) | Memory: 12.5 MB (46.00%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,19 +10,21 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        listR = ListNode()
-        l = listR
-        while list1 != None and list2 != None:
+        RList = ListNode()
+        cur = RList
+
+        while list1 and list2:
             if list1.val < list2.val:
-                l.next = ListNode(list1.val)
-                l = l.next
+                cur.next = ListNode(list1.val)
                 list1 = list1.next
             else:
-                l.next = ListNode(list2.val)
-                l = l.next
+                cur.next = ListNode(list2.val)
                 list2 = list2.next
-        if list1 == None:
-            l.next = list2
-        if list2 == None:
-            l.next = list1
-        return listR.next
+            cur = cur.next
+
+        if list1:
+            cur.next = list1
+        elif list2:
+            cur.next = list2
+        
+        return RList.next
